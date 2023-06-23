ad0bbac1927b74ba94d5ce155e2ba434fc2ee8d4
shivamxaryan <shivamaryan64@gmail.com>
2023-06-23 13:29:24 +0530

    updatinng header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -36,7 +36,42 @@ const Header = () => {
 
   return (
     <div className="sticky top-0 z-10 flex flex-row items-center justify-between h-14 px-4 md:px-5 bg-white dark:bg-black">
-      
+      {loading && <Loader />}
+
+      <div className="flex h-5 items-center">
+        {pageName !== "video" && (
+          <div 
+          className="flex md:hidden md:mr-6 cursor-pointer items-center justify-center h-10 w-10 rounded-full hover:bg-[#303030]/[0.6]" 
+          onClick={mobileMenuToggle}
+          >
+            {mobileMenu ? (<CgClose className="text-white text-xl" />
+            ) : (
+                <SlMenu className="text-white text-xl" />
+            )}
+            
+          </div>
+        )}
+
+        <Link to="/" className="flex h-5 items-center">
+        <img 
+        className="h-full hidden dark:md:block" 
+        src={ytLogo} alt="PlayTube"
+        />
+
+        <img 
+        className="h-full md:hidden" 
+        src={ytLogo} alt="PlayTube"
+        />
+        </Link>
+        
+        <div className="group flex items-center">
+          <div className="flex h-8 md:h-10 md:ml-10 md:pl-5 border border-[#303030] rounded-l-3xl ground-focus-within:border-blue-500 md:group-focus-within:ml-5 md:group-focus-within:pl-0">
+          <div className="w-10 items-center justify-center hidden group-focus-witin:md:flex">
+            <IoIosSearch className="text-white text-xl" />
+          </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }

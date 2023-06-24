ce32544cd1a98dd137ef3e79d4840bf094f4e06f
shivamxaryan <shivamaryan64@gmail.com>
2023-06-25 00:00:08 +0530

    creating left category section

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useContext, useEffect } from "react";
 
+import { Context } from "../context/contextApi";
+import LeftNav from "./LeftNav";
 const Feed = () => {
+  const { loading, searchResults } = useContext(Context);
+
+    useEffect(() => {
+        document.getElementById("root").classList.remove("custom-h");
+    }, []);
   return (
     <div>
-      
+              <div className="flex flex-row h-[calc(100%-56px)]">
+              <LeftNav />
+            <div className="grow w-[calc(100%-240px)] h-full overflow-y-auto bg-black">
+
+            </div>
+        </div>
     </div>
   )
 }

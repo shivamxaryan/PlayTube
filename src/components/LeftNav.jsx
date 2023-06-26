2790ae7df64d01d5b86fad94b489ca91238a0d35
shivamxaryan <shivamaryan64@gmail.com>
2023-06-26 13:39:39 +0530

    creating a component LeftNavMenuItem

--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -9,6 +9,21 @@ const LeftNav = () => {
   const { selectedCategory, setSelectedCategory, mobileMenu } =
     useContext(Context);
 
+    const navigate = useNavigate();
+
+    const clickHandler = (name, type) => {
+        switch (type) {
+            case "category":
+                return setSelectedCategory(name);
+            case "home":
+                return setSelectedCategory(name);
+            case "menu":
+                return false;
+            default:
+                break;
+        }
+    };
+
   return (
     <div
       className={`md:block w-[240px] overflow-y-auto h-full py-4 bg-black absolute md:relative z-10 translate-x-[-240px] md:translate-x-0 transition-all ${
@@ -22,7 +37,10 @@ const LeftNav = () => {
               <LeftNavMenuItem
                 text={item.type === "home" ? "Home" : item.name}
                 icon={item.icon}
-                action={() => {}}
+                action={() => {
+                  clickHandler(item.name, item.type);
+                  navigate("/");
+                }}
                 className={`${
                   selectedCategory === item.name ? "bg-white/[0.15]" : ""
                 }`}

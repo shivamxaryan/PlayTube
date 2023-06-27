b40c5f4800d73c19c37e68a666a3be02e66fc658
shivamxaryan <shivamaryan64@gmail.com>
2023-06-27 13:14:56 +0530

    Creating video cards

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import { abbreviateNumber } from "js-abbreviation-number";
+import { Link } from "react-router-dom";
+import { BsFillCheckCircleFill } from "react-icons/bs";
 
-const VideoCard = () => {
+import VideoLength from "../shared/videoLength";
+
+const VideoCard = ({ video }) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Link to={`/video/${video?.videoId}`}>
+      <div className="flex flex-col mb-8">
+        <div className="relative h-48 md:h-40 md:rounded-xl overflow-hidden">
+          <img
+            className="h-full w-full object-cover"
+            src={video?.thumbnails[0]?.url}
+          />
 
-export default VideoCard
+          {video?.lengthSeconds && <VideoLength time={video?.lengthSeconds} />}
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default VideoCard;

8a4b0dbea0de688a8c4a339c3d05a2abce59d310
shivamxaryan <shivamaryan64@gmail.com>
2023-06-27 19:46:24 +0530

    creating video details and its cards

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -6,30 +6,27 @@ import VideoCard from "./VideoCard";
 const Feed = () => {
   const { loading, searchResults } = useContext(Context);
 
-    useEffect(() => {
-        document.getElementById("root").classList.remove("custom-h");
-    }, []);
+  useEffect(() => {
+    document.getElementById("root").classList.remove("custom-h");
+  }, []);
+
   return (
     <div>
-              <div className="flex flex-row h-[calc(100%-56px)]">
-              <LeftNav />
-            <div className="grow w-[calc(100%-240px)] h-full overflow-y-auto bg-black">
-
-            {!loading &&
-                        searchResults.map((item) => {
-                            if (item.type !== "video") return false;
-                            return (
-                                <VideoCard
-                                    key={item?.video?.videoId}
-                                    video={item?.video}
-                                />
-                            );
-                        })}
-
-            </div>
+      <div className="flex flex-row h-[calc(100%-56px)]">
+        <LeftNav />
+        <div className="grow w-[calc(100%-240px)] h-full overflow-y-auto bg-black">
+          {!loading &&
+            searchResults.map((item) => {
+              if (item.type !== "video") return false;
+              return (
+                <VideoCard key={item?.video?.videoId} video={item?.video} />
+              );
+            })}
         </div>
+      </div>
     </div>
-  )
-}
+  );
+  
+};
 
-export default Feed
+export default Feed;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,6 @@ import React, { useContext, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 
 import ytLogo from "../images/yt-logo.png";
-import ytLogoMobile from "../images/yt-logo-mobile.png";
-
 import { SlMenu } from "react-icons/sl";
 import { IoIosSearch } from "react-icons/io";
 import { RiVideoAddLine } from "react-icons/ri";
